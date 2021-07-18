ea55f86f1df4fde1635be51b4bb7026534195e7a
Kade Griffith <kadeg95@gmail.com>
2021-07-18 12:53:17 -0600

    added a few styles into css, as well as adding new links in projects page, along with real screenshots of the site reaplacing filler images

---
+++ PROJECTS/scorekeeper.js
@@ -0,0 +1,69 @@
+const player1 = {
+  score: 0,
+  button: document.querySelector("#player1"),
+  display: document.querySelector("#score1"),
+};
+const player2 = {
+  score: 0,
+  button: document.querySelector("#player2"),
+  display: document.querySelector("#score2"),
+};
+
+const resetbtn = document.querySelector("#reset");
+const maxScoreSelect = document.querySelector("#maxScore");
+let maxScore = parseInt(maxScoreSelect.value);
+let isGameOver = false;
+
+function updateScore(player, opponent) {
+  if (!isGameOver) {
+    player.score += 1;
+    player.display.innerText = player.score;
+  }
+  if (player.score === maxScore) {
+    isGameOver = true;
+    opponent.display.classList.add("has-text-danger");
+    player.display.classList.add("has-text-success");
+    player.button.disabled = true;
+    opponent.button.disabled = true;
+  }
+  if (player.score === opponent.score && player.score === maxScore - 1) {
+    maxScoreSelect.classList.add("tiebreaker");
+    maxScore++;
+    maxScoreSelect[0].innerText = `Win with ${maxScore} points!`;
+  }
+}
+
+player1.button.addEventListener("click", function (e) {
+  updateScore(player1, player2);
+});
+
+player2.button.addEventListener("click", function (e) {
+  updateScore(player2, player1);
+});
+
+maxScoreSelect.addEventListener("change", function () {
+  maxScore = parseInt(this.value);
+  reset();
+});
+
+resetbtn.addEventListener("click", reset);
+
+function reset(index) {
+  if ((isGameOver = true)) {
+    score1.innerText = 0;
+    score2.innerText = 0;
+    player1.score = 0;
+    player2.score = 0;
+    score2.classList.remove("has-text-success", "has-text-danger");
+    score1.classList.remove("has-text-success", "has-text-danger");
+    isGameOver = false;
+    player1.button.disabled = false;
+    player2.button.disabled = false;
+    maxScoreSelect.classList.remove("tiebreaker");
+    maxScoreSelect.selectedIndex = index;
+    for (let i = 0; i <= 6; i++) {
+      maxScoreSelect[i].value = 5 + i;
+      maxScoreSelect[i].innerText = 5 + i;
+    }
+  }
+}
